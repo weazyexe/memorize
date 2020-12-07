33f95c026908bf7b183d899f2a82cf8a1b1d49d0
Timur Zadvornov <weazy.exe@gmail.com>
2020-12-07 18:23:02 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Timur Zadvornov on 07.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
