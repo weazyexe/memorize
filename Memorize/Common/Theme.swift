e492c1d55e6108f6bf1abbeca2db5cbb7d160c3e
Timur Zadvornov <weazy.exe@gmail.com>
2021-01-07 13:39:57 +0300

    add theme constants

---
+++ Memorize/Common/Theme.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeManager.swift
+//  Memorize
+//
+//  Created by Timur Zadvornov on 07.01.2021.
+//
+
+import Foundation
+import SwiftUI
+
+class Theme {
+    
+    
+    
+    static var primaryColor: Color = DEFAULT_COLOR
+    
+    
+}
--- Memorize/Util/Const.swift
+++ Memorize/Util/Const.swift
@@ -5,8 +5,21 @@
 //  Created by Timur Zadvornov on 06.01.2021.
 //
 
-import Foundation
+import SwiftUI
 
 let PAIRS_COUNT_RANGE = 2...5
 
-let CARD_EMOJIS: [String] = ["😳", "😎", "🗿", "🌚", "🤔"]
+let DEFAULT_COLOR = Color(red: 255, green: 93, blue: 82)
+let DEFAULT_EMOJI_SET = ["😳", "😎", "🗿", "🌚", "🤔"]
+
+let COLORS: [Color] = [
+    DEFAULT_COLOR,
+    Color(red: 255, green: 177, blue: 82),
+    Color(red: 82, green: 186, blue: 255)
+]
+
+let EMOJI_SETS: [[String]] = [
+    DEFAULT_EMOJI_SET,
+    ["💕", "🎉", "❄️", "🎁", "💨"],
+    ["🤯", "🤡", "👀", "🧑‍💻", "🐸"]
+]
